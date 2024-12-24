a0adc20deb1fb1b540e90516c21668ba6aa3c3d4
mkowalew <martin.kowalewski@att.net>
2024-12-24 15:57:48 -0600

    increase rate limiter during testing

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ const app = express();
 const PORT = 4000;
 
 const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 15 minutes
-    limit: 10, // Limit each IP to 10 requests per `window` (here, per 5 minutes).
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 })
 
 // Enable CORS
-app.use(cors())
+app.use(cors(), limiter)
 
 // Use routes
 app.use('/api/saleslegacy', salesRoutes);
