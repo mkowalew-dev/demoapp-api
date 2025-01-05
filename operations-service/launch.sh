9e8c85f79a4dd273c8541c9c120750777252454d
mkowalew <martin.kowalewski@att.net>
2025-01-05 12:46:46 -0600

    update operations service launch.sh

--- operations-service/launch.sh
+++ operations-service/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export OTEL_INSTRUMENTATION_COMMON_DEFAULT_ENABLED=true
 export SPLUNK_METRICS_ENABLED=true
-export OTEL_RESOURCE_ATTRIBUTES=deployment.environment=development,service.version=2.0
+export OTEL_RESOURCE_ATTRIBUTES=deployment.environment=staging,service.version=2.0
 export OTEL_SERVICE_NAME=operations-service
 export SPLUNK_PROFILER_ENABLED=true
 export SPLUNK_PROFILER_MEMORY_ENABLED=true
